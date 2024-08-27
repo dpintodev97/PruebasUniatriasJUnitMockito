5514fad1c422a3ed2ea60c6337f1c547f941eeb3
DAVID <davidpintodev97@gmail.com>
2024-08-27 16:53:34 +0200

    añadidos Interfaces ServicioA, ServicioB

---
+++ src/main/java/services/ServicioA.java
@@ -0,0 +1,10 @@
+package services;
+/**
+ * //METODO ABSTRACTO (polimorfismo); PARA QUE ESTA INTERFACE DEFINA QUÉ PUEDE HACER UN OBJETO, 
+ * 		PERO SEAN LAS MÚLTIPLES CLASES QUE LA IMPLEMENTEN LAS QUE DEFINAN CÓMO PUEDE HACERLO
+ */
+public interface ServicioA {
+	
+	public abstract int sumar(int a, int b);
+
+}
---
+++ src/main/java/services/ServicioB.java
@@ -0,0 +1,25 @@
+package services;
+/***
+ * 1.ServicioB (interface) DEPENDE DE ServicioA (interface) PARA ACCEDER A LOS SERVCICIOS QUE ALLÍ TENGA, EN ESTE CASO,
+ * 	EL METODO ABTSRACTO DE sumar ; POR ESO AQUÍ ESCRIBIMOS UN getServicioA();
+ * 
+ * 2.MÉTODO void setServicioA(9; : PARA ESTABLECER QUE ESTE ServicioB DEPENDE DE OTRA CLASE, DEL MÉTODO DE OTRA CLASE
+ * 
+ * 3.METODO PROPIO multiplicarSumar CON 3 PARAMETROS; PARA QUE PUEDA MULTIPLICAR LA SUMA DE ESOS DOS VALORES int a, int b
+ * 
+ * 4.METODO MULTIPLICAR, int a, int b
+ */
+public interface ServicioB {
+	//TOMAR METODO DE ServicioA:
+	public ServicioA getServicioA();
+	
+	//DEPENDE DE METODO DE OTRA CLASE
+	public void setServicioA(ServicioA servicioA);
+	
+	public int multiplicarSumar(int a, int b, int multiplicador);
+	
+	public int multiplicar(int a, int b);
+	
+	
+
+}
